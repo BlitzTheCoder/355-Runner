9bf960308b321c2c0d0370e21946deb13f46dbbc
Unknown <brown.r1992@gmail.com>
2018-09-04 21:50:11 -0400

    Started Setup on Wall Spawning

--- Assets/SceneController.cs
+++ Assets/SceneController.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class SceneController : MonoBehaviour {
 
+    public GameObject prefabObstacle;
     public GameObject prefabWall;
+    private List<GameObject> prefabWalls = new List<GameObject>();
     float delayUntilSpawn = 0;
+    float wallTimer = 0;
 
 	void Start () {
 		
@@ -15,11 +18,21 @@ public class SceneController : MonoBehaviour {
 	void Update () {
 
         delayUntilSpawn -= Time.deltaTime;
+        wallTimer -= Time.deltaTime;
         if (delayUntilSpawn <= 0)
         {
             Vector3 pos = new Vector3(0, 0, 20);
-            Instantiate(prefabWall, pos, Quaternion.identity);
+            Instantiate(prefabObstacle, pos, Quaternion.identity);
             delayUntilSpawn = Random.Range(1, 3);
         }
-	}
+
+        for (int i = prefabWalls.Count; i < 10; i++)
+        {
+            float nextWallZ = prefabWalls[i].transform.position.z * 7.5f;
+            Vector3 pos = new Vector3(-7, 0, nextWallZ + 20);
+            prefabWalls.Add(prefabWall);
+            Instantiate(prefabWalls[i], pos, Quaternion.identity);
+            wallTimer = 1;
+        }
+    }
 }
--- Assets/WallController.cs
+++ Assets/WallController.cs
@@ -2,22 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ObstacleMovement : MonoBehaviour {
+public class WallController : MonoBehaviour {
 
     const float speed = -10;
 
-	// Use this for initialization
-	void Start () {
+    void Start () {
 		
 	}
 	
-	// Update is called once per frame
 	void Update () {
+
         transform.position += new Vector3(0, 0, speed) * Time.deltaTime;
 
         if (transform.position.z < -20)
         {
             Destroy(this);
         }
-	}
+
+    }
 }
